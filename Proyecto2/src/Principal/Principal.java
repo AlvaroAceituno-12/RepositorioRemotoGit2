76b0741587d7cf5877a7dd1b1c7039bc482fdf67
jagmp <jagmp@Alvaro>
2026-02-17 16:19:35 +0100

    cambio en rama1

--- Proyecto2/src/Principal/Principal.java
+++ Proyecto2/src/Principal/Principal.java
@@ -4,7 +4,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Este es el mensaje principal del texto");
+		System.out.println("Este es el mensaje desde Rama1");
+		System.out.println("Este es otro mensaje desde Rama1");
+		
 
 	}
 
